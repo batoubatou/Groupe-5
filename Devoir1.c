936a769257e7c091f8964bdc1b91f82e9f7f22e5
oundakoula <alixoundakoula01@icloud.com>
2025-06-15 22:24:31 +0000

    Create Devoir1.c

---
+++ Devoir1.c
@@ -0,0 +1,24 @@
+//definir la sctucture facture par Brelle
+
+//Procédure remplir tab par Brelle
+
+
+//Procédure affiche tab par Brelle
+
+
+//Fonction somme tab par Alix
+
+
+//Fonction Min tab par Alix
+
+
+//Fonction Max tab par Alix
+
+
+//Fonction somme par objet par Davide
+
+
+//Procédure facture par étudiant par Davide
+
+
+//Main (pour intégrer les fonctionnalités du programme par Davide
