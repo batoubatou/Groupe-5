5669ef46736d73fb9194c2f8e6452d2dfdcdc5eb
Brelle <moueleyembi@gmail.com>
2025-06-16 00:11:59 +0000

    Update Devoir1.c

--- Devoir1.c
+++ Devoir1.c
@@ -5,24 +5,44 @@
 typedef struct Devoir1
 {
     int numero
-    int id_etudiant;
+    int id;
 float montant,inscription,scolarite;
 char objet[5];
 
-
-    /* data */
 } Facture;
 
 
 //Procédure remplir tab factures par Brelle 
-void remplirFactures(Facture tab [], int n) {
+void remplir Factures (struct Facture Tab[2],int n) {
+
+ for (int i = 0; i < n; i++)
+ {
+    printf ("\n veuillez saisir le numero de l etudiant \n");
+    scanf("%d",&Facture Tab [i].numero);
+     printf ("\n veuillez saisir l id de l etudiant \n");
+     scanf("%d",&Facture Tab [i].id);
+     printf ("\n veuillez saisir le montant de l etudiant \n");
+     scanf("%f",&Facture Tab [i].montant);
+     printf ("\n veuillez saisir l inscription de l etudiant \n");
+     scanf("%f",&Facture Tab [i].incription);
+     printf ("\n veuillez saisir la scolarite de l etudiant \n");
+     scanf("%f",&Facture Tab [i].scolarite);
+     printf ("\n veuillez saisir l objet \n");
+     scanf("%s",&Facture Tab [i].objet);
+
+
+
+
+
+
+ }
+ 
 
 }
 
 
-
-
 //Procédure affiche tab par Brelle
+void afficher (Facture)
 
 
 //Fonction somme tab par Alix
